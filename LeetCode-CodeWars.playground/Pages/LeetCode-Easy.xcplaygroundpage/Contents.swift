8087a7ed1a5c25b3086e2a1ee57832c443788d11
Shlyakhovenko Vladislav <homyak603@gmail.com>
2023-07-01 02:07:25 +0200

    Solved Two Sum

--- LeetCode-CodeWars.playground/Pages/LeetCode-Easy.xcplaygroundpage/Contents.swift
+++ LeetCode-CodeWars.playground/Pages/LeetCode-Easy.xcplaygroundpage/Contents.swift
@@ -2,7 +2,19 @@
 import Foundation
 
 /*
- Here are all the solved problems from Leetcode easy
+ Here are all the solved problems from Leetcode easy 1/690
  */
 
-print(1)
+
+// MARK: - Two Sum: https://leetcode.com/problems/two-sum/
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var dict: [Int:Int] = [:]
+    for (key,value) in nums.enumerated() {
+        if dict[target-value] != nil {
+            return [dict[target-value, default: 0], key]
+        }
+        dict[value] = key
+    }
+    return [0,0]
+}
