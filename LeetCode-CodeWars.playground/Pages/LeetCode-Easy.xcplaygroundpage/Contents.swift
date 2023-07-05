834948e97916f6400411c34609e0916e05ee7d49
Shlyakhovenko Vladislav <homyak603@gmail.com>
2023-07-05 21:39:55 +0200

    Solved Missing number problem

--- LeetCode-CodeWars.playground/Pages/LeetCode-Easy.xcplaygroundpage/Contents.swift
+++ LeetCode-CodeWars.playground/Pages/LeetCode-Easy.xcplaygroundpage/Contents.swift
@@ -18,3 +18,10 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     }
     return [0,0]
 }
+
+// MARK: - Missing Number: https://leetcode.com/problems/missing-number/
+
+func missingNumber(_ nums: [Int]) -> Int {
+    let n = nums.count
+    return n * (n + 1 ) / 2 - nums.reduce(0,+)
+}
